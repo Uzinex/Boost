dc1609159ebe405ae590e7011668988f580a534f
Uzinex <uzdilovferuz@gmail.com>
2025-10-15 11:16:50 +0500

    Fix Telegram bot initialization

--- apps/backend/bot/__init__.py
+++ apps/backend/bot/__init__.py
@@ -1,5 +1,4 @@
 from aiogram import Bot, Dispatcher
-
 from aiogram.enums import ParseMode
 
 from core.config import settings
@@ -7,5 +6,6 @@ from core.config import settings
 from bot.app.service.bot_service import router as bot_router
 
 
+bot = Bot(token=settings.TELEGRAM_BOT_TOKEN, parse_mode=ParseMode.HTML)
 dp = Dispatcher()
 dp.include_router(bot_router)
